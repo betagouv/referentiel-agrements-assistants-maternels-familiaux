35c3754f9e12b0d6d7269058662ab8a0718bda59
Pierre TOP (TOPI) <pierre.top@octo.com>
2026-09-08 11:58:48 +0200

    tech(api): ajouter le framework de lint

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import js from '@eslint/js';
+import json from '@eslint/json';
+import commentsPlugin from '@eslint-community/eslint-plugin-eslint-comments/configs';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import chaiExpectPlugin from 'eslint-plugin-chai-expect';
+import mochaPlugin from 'eslint-plugin-mocha';
+import simpleImportPlugin from 'eslint-plugin-simple-import-sort';
+import eslintPluginYml from 'eslint-plugin-yml';
+
+export default defineConfig([
+  { linterOptions: { reportUnusedDisableDirectives: 'error' } },
+  commentsPlugin.recommended,
+  {
+    files: ['**/*.{js,mjs}'],
+    plugins: { js, 'simple-import-sort': simpleImportPlugin },
+    extends: ['js/recommended'],
+    languageOptions: { ecmaVersion: 2026, sourceType: 'module' },
+    rules: {
+      'no-console': 'error',
+      'no-empty-function': 'error',
+      'no-var': ['error'],
+      'prefer-const': ['error'],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
+  ...eslintPluginYml.configs.recommended,
+  {
+    ...mochaPlugin.configs.recommended,
+    files: ['tests/**/*.js'],
+    rules: {
+      ...mochaPlugin.configs.recommended.rules,
+      'mocha/no-exclusive-tests': 'error',
+      'mocha/no-pending-tests': 'error',
+    },
+  },
+  chaiExpectPlugin.configs['recommended-flat'],
+  globalIgnores(['package-lock.json']),
+]);
